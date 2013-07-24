dd80b043ee0a5e45cb9fd5458aaad602695a7ecb
Shawn Hoerner <shoerner@error.local>
2013-07-24 02:15:00 -0400

    Modular implementation.

--- purl.pl
+++ purl.pl
@@ -2,34 +2,85 @@
 use warnings;
 use strict;
 use IO::Socket::INET;
+use Getopt::Long;
 
 #Flush after every write
 $| = 1;
 
 my $socket;
 my $client_socket;
-
-$socket = new IO::Socket::INET (
-	PeerHost => 'shoerner.net',
-	PeerPort => 80,
-	Proto => 'tcp',
-) or die "ERROR in socket creation: $!\n";
-
-$socket->send("GET / HTTP/1.0\n\n");
-
-my @output;
+my ($port, $host, $path);
+my @HTMLDump;
 my $HTMLStart;
-while ( <$socket> ) 
-{ 
-	push(@output, $_); 
-	if(substr($_, 0, 9) eq "<!DOCTYPE")
+
+sub seperateURL
+{
+	my $incomingURL = join('', @_);
+
+	#get addressing port 
+	index($incomingURL, 'https://') != -1 ? $port = 443 : $port = 80;
+
+	#Get addressing hostname
+	if(index($incomingURL, '://') != -1)
 	{
-		$HTMLStart = scalar(@output)-1;
+		my $typeEnd = index($incomingURL, '://')+3;
+		if(index($incomingURL, '/', $typeEnd) != -1)
+		{
+			$host = substr($incomingURL, $typeEnd, (index($incomingURL, '/', $typeEnd)-$typeEnd));
+			$path = substr($incomingURL, index($incomingURL, '/', $typeEnd))
+		}
+		else
+		{
+			$host = substr($incomingURL, $typeEnd);
+			$path = '';
+		}
+		
+	}
+	else
+	{
+		$host = substr($incomingURL, 0, (index($incomingURL, '/')));
+		if(index($incomingURL, '/') != -1)
+		{
+			$path = substr($incomingURL, index($incomingURL, '/'))
+		}
+		else
+		{
+			$path = '';
+		}
+	}
+
+	print "Port: $port\n";
+	print "Host: $host\n";
+	print "Path: $path\n";
+}
+sub performRequest
+{
+	$socket = new IO::Socket::INET (
+		PeerHost => $host,
+		PeerPort => $port,
+		Proto => 'tcp',
+	) or die "Error in socket creation: $!\n";
+
+	$socket->send("GET $path HTTP/1.0\n\n");
+
+	while ( <$socket> ) 
+	{ 
+		push(@HTMLDump, $_); 
+		if(substr($_, 0, 9) eq "<!DOCTYPE")
+		{
+			$HTMLStart = scalar(@HTMLDump)-1;
+		}
+	}
+	close $socket;
+}
+sub printHTML
+{
+	for(my $counter = $HTMLStart; $counter < @HTMLDump; $counter++)
+	{
+		print $HTMLDump[$counter];
 	}
 }
-close $socket;
 
-for(my $counter = $HTMLStart; $counter < @output; $counter++)
-{
-	print $output[$counter];
-}
+seperateURL($ARGV[0]);
+performRequest();
+printHTML();
